be5c9169ce8093c85fa31af819f00db61da5d70d
69VR69 <44296242+69VR69@users.noreply.github.com>
2023-10-16 19:48:00 +0200

    Fix bugs

--- server_manager.py
+++ server_manager.py
@@ -12,7 +12,6 @@ class ServerManager:
     def start(self):
         print("server starting...")
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.settimeout(10)
         print("connecting to server ("+self.ip + "/" + str(self.port) + ")...")
         self.socket.connect((self.ip, self.port))
         print("connected to server")
