e75c276872eee47e25d4860e6e240a3de131bd78
69VR69 <44296242+69VR69@users.noreply.github.com>
2023-10-22 12:23:52 +0200

    init neural network

--- model.py
+++ model.py
@@ -1,9 +1,13 @@
+from neural_network import QNetwork
 import random
+import torch
 
 class Model:
-    def __init__(self, epsilon = 0.99, num_actions = 9):
+    def __init__(self, epsilon = 0.99, num_state = 27,num_actions = 9):
         self.epsilon = epsilon
+        self.num_state = num_state
         self.num_actions = num_actions
+        self.q_network = QNetwork(num_state, num_actions)
 
     def get_action(self, state):
         # get a random number between 0 and 1
@@ -15,4 +19,5 @@ class Model:
             return self.get_best_action(state)
         
     def get_best_action(self, state):
-        return 0 # TODO call the model to get the best action for the given state
+        # get the best action for the state
+        return  self.q_network.forward(state).argmax().item()
---
+++ neural_network.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import random
+
+class QNetwork:
+    def __init__(self, state_size, action_size, fc1_units=64, fc2_units=64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state (37)
+            action_size (int): Dimension of each action (4)
+            fc1_units (int): Number of nodes in first hidden layer (64)
+            fc2_units (int): Number of nodes in second hidden layer (64)
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = torch.manual_seed(random.randint(0, 1000))
+        self.fc1_units = fc1_units
+        self.fc2_units = fc2_units
+
+        # Q-Network
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
